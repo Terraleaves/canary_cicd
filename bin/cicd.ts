677d2428db0d1ce3863d309fd394d482985ca565
Kiyohiro Kambayashi <k.kambayashi@westernsydney.edu.au>
2024-09-17 22:41:55 +1000

    Initial commit

---
+++ bin/cicd.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { CicdStack } from '../lib/cicd-stack';
+
+const app = new cdk.App();
+new CicdStack(app, 'CicdStack', {
+  env: {
+    account: '',
+    region: 'ap-southeast-2'
+  }
+});
