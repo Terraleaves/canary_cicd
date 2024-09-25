9d8b7ad7bc454904eacf59064e1beb7ba25e1f9c
Kiyohiro Kambayashi <k.kambayashi@westernsydney.edu.au>
2024-09-26 03:54:12 +1000

    Modify cicd-stack.ts file

--- lib/cicd-stack.ts
+++ lib/cicd-stack.ts
@@ -1,7 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep, ManualApprovalStep } from 'aws-cdk-lib/pipelines';
-import { DevOpsStack } from './devops-stack';
 import { MyPipelineAppStage } from './pipeline-app-stage';
 
 export class CicdStack extends cdk.Stack {
@@ -12,7 +11,7 @@ export class CicdStack extends cdk.Stack {
       input: CodePipelineSource.connection('kiyohiro0310/devops_cicd', 'master', {
         connectionArn: "arn:aws:codestar-connections:us-east-2:325861338157:connection/dc5275a2-85db-48f1-91e2-a1aac8496373"
       }),
-      commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+      commands: ['npm ci', 'npm run build', 'npx cdk synth', 'npm test'],
       primaryOutputDirectory: 'cdk.out'
     })
 
@@ -21,12 +20,6 @@ export class CicdStack extends cdk.Stack {
       synth: synthStep
     });
 
-    const testStep = new ShellStep('Test', {
-      input: synthStep,  // Use the output of the synth step
-      commands: ['npm test'],  // Run your tests
-    });
-
-
     // Deploy stage
     const deployStage = pipeline.addStage(new MyPipelineAppStage(this, "Deploy", {
       env: {
@@ -34,8 +27,6 @@ export class CicdStack extends cdk.Stack {
         region: 'ap-southeast-2'
       }
     }));
-
-    deployStage.addPre(testStep);
 
     deployStage.addPre(new ManualApprovalStep('approval'));
 
