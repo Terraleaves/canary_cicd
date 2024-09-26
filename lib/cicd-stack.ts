846f547707a5f73d6cb48ead4868673b14010ebf
Kiyohiro Kambayashi <k.kambayashi@westernsydney.edu.au>
2024-09-26 15:31:20 +1000

    Modify

--- lib/cicd-stack.ts
+++ lib/cicd-stack.ts
@@ -1,59 +1,58 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep, ManualApprovalStep } from 'aws-cdk-lib/pipelines';
-import { MyPipelineAppStage } from './pipeline-app-stage';
-import { ComputeType } from 'aws-cdk-lib/aws-codebuild';
-import { TestStage } from './test-app-stage';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+  ManualApprovalStep,
+} from "aws-cdk-lib/pipelines";
+import { MyPipelineAppStage } from "./pipeline-app-stage";
+import { ComputeType } from "aws-cdk-lib/aws-codebuild";
 
 export class CicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const synthStep = new ShellStep('Synth', {
-      input: CodePipelineSource.connection('kiyohiro0310/devops_cicd', 'master', {
-        connectionArn: "arn:aws:codestar-connections:us-east-2:325861338157:connection/dc5275a2-85db-48f1-91e2-a1aac8496373"
-      }),
-      commands: ['npm ci', 'npm run build', 'npx cdk synth'],
-      primaryOutputDirectory: 'cdk.out'
-    })
-
-    const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'DevOpsPipeline',
-      synth: synthStep,
-      codeBuildDefaults: {
-        buildEnvironment: {
-          computeType: ComputeType.LARGE
+    const synthStep = new ShellStep("Synth", {
+      input: CodePipelineSource.connection(
+        "kiyohiro0310/devops_cicd",
+        "master",
+        {
+          connectionArn:
+            "arn:aws:codestar-connections:us-east-2:325861338157:connection/dc5275a2-85db-48f1-91e2-a1aac8496373",
         }
-      }
+      ),
+      commands: ["npm ci", "npm run build", "npx cdk synth"],
+      primaryOutputDirectory: "cdk.out",
     });
 
-    // Test stage
-    pipeline.addStage(new TestStage(this, "Test", {
-      env: {
-        account: '325861338157',
-        region: 'ap-southeast-2'
-      }
-    }))
+    const pipeline = new CodePipeline(this, "Pipeline", {
+      pipelineName: "DevOpsPipeline",
+      synth: synthStep,
+    });
 
     // Deploy stage
-    const deployStage = pipeline.addStage(new MyPipelineAppStage(this, "Deploy", {
-      env: {
-        account: '325861338157',
-        region: 'ap-southeast-2'
-      }
+    const deployStage = pipeline.addStage(
+      new MyPipelineAppStage(this, "Deploy", {
+        env: {
+          account: "325861338157",
+          region: "ap-southeast-2",
+        },
+      })
+    );
+
+    deployStage.addPre(new ShellStep("RunTests", {
+      commands: ["npm ci", "npm test"],
     }));
 
-    deployStage.addPre(new ManualApprovalStep('approval'));
+    deployStage.addPost(new ManualApprovalStep("approval"));
 
-    const wave = pipeline.addWave('wave');
-    wave.addStage(new MyPipelineAppStage(this, 'Deploy to EU', {
-      env: { account: '325861338157', region: 'eu-west-1' }
-    }));
-    wave.addStage(new MyPipelineAppStage(this, 'Deployt to US', {
-      env: { account: '325861338157', region: 'us-west-1' }
-    }));
-
+    // const wave = pipeline.addWave('wave');
+    // wave.addStage(new MyPipelineAppStage(this, 'AppEU', {
+    //   env: { account: '325861338157', region: 'eu-west-1' }
+    // }));
+    // wave.addStage(new MyPipelineAppStage(this, 'AppUS', {
+    //   env: { account: '325861338157', region: 'us-west-1' }
+    // }));
   }
 }
-
-
